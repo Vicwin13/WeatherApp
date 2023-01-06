3e2830c7aab659cc6e529d2f4ead0c2ade5b10df
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2023-01-06 20:44:40 +0000

    bugfix: add optional chaining to prevent access bugs

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -4,29 +4,38 @@ import { useState } from "react";
 
 function Head() {
   const apiKey = "0f7ccf743853d94ac08d56cefbe74d7d";
-  const [weatherData, setWeatherData] = useState([{}]);
+  const [weatherData, setWeatherData] = useState({});
   const [city, setCity] = useState("");
   const [humidity, setHumidity] = useState("");
   const [wind, setWind] = useState("");
   const [visibility, setVisibility] = useState("");
   const [pressure, setPressure] = useState("");
+  const [loading, setLoading] = useState(false)
 
-  const getWeather = (event) => {
-    if (event.key == "Enter") {
-      fetch(
+
+
+  const getWeather = async (event) => {
+    event.preventDefault()
+    setLoading(true)
+
+    try {
+      const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`
       )
-        .then((response) => response.json())
-        .then((data) => {
-          setWeatherData(data);
-          setCity("");
-          answer();
-        });
+      const data = await response.json()
+      setWeatherData(data);
+      setCity("");
+      answer();
+
+    } catch (error) {
+      console.log(error.message)
+    } finally {
+      setLoading(false)
     }
   };
 
   const answer = () => {
-    setHumidity(weatherData.main.humidity);
+    setHumidity(weatherData?.main?.humidity);
     ("Pls let this work");
     // setWind(weatherData.wind.speed);
     // setVisibility(weatherData.visibility);
@@ -40,34 +49,42 @@ function Head() {
           <h1 className="text-2xl font-bold">MyWeather</h1>
         </div>
         <div>
-          <input
-            className="border-2"
-            type="text"
-            onChange={(e) => setCity(e.target.value)}
-            placeholder="Enter City..."
-            value={city}
-            onKeyUp={getWeather}
-          />
+          <form onSubmit={getWeather}>
+
+            <input
+              className="border-2"
+              type="text"
+              onChange={(e) => setCity(e.target.value)}
+              placeholder="Enter City..."
+              value={city}
+              style={{marginRight: "1rem", padding: "3px 10px"}}
+            />
+
+            <button style={{border: "1px solid grey", padding: "3px 10px"}} type="submit">Submit</button>
+          </form>
         </div>
       </div>
+      
+      {loading && <p>Loading...</p>}
+
       <div className=" bg-lime-400 px-2 py-6  w-full rounded-t-lg mt-8">
         <div className="pb-4">
-          <p className="text-4xl font-bold ">{weatherData.name} Location </p>
+          <p className="text-4xl font-bold ">{weatherData?.name} Location </p>
         </div>
         <div className="flex gap-3">
           <p className="text-xl font-semibold">
-            {weatherData.main.temp} &deg;F
+            {weatherData?.main?.temp} &deg;F
           </p>
           <p className="text-xl font-semibold">
-            {weatherData.weather[0].main} condition
+            {weatherData?.weather?.[0]?.main} condition
           </p>
         </div>
       </div>
 
       <Features getWeather={humidity} />
-      {/* <button className="bg-yellow-500 p-4 rounded" onClick={() => answer()}>
+      <button className="bg-yellow-500 p-4 rounded" onClick={() => answer()}>
         Click me
-      </button> */}
+      </button>
     </div>
   );
 }
